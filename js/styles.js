3c630bd6330c483f62870b5da1d8bb6e9af49911
Nikhil Rao <sourcenrao@gmail.com>
2021-02-18 22:43:08 -0500

    Write main page with js on load. Toggle title on click.

--- js/styles.js
+++ js/styles.js
@@ -1,2 +1,25 @@
-document.getElementsById("title").innerHTML = "The Source Flows";
-alert("This alert box was called with the onload event");
+var frontpage = `<div class="col1">
+<h2>First Website</h2>
+<p>This is a testing ground for Nikhil Rao's first website. Feel free to look around, maybe you'll find something interesting!</p>
+</div>
+<div class="col2">
+<h2>About</h2>
+<p>Utility Mover attempts to estimate your utility costs after a move using current information and the destination location. Thanks to Code Louisville!</p>
+</div>`
+
+var toggletitle = 0;
+
+$( "h1" ).click(function() {
+    if(toggletitle == 0){
+    document.getElementById("title").innerHTML = "The Source Flows";
+    toggletitle = 1;
+    }
+    else {
+        document.getElementById("title").innerHTML = "Welcome to the Source";
+        toggletitle = 0;
+    }
+});
+
+$( ".text" ).html(frontpage);
+
+// $( "" )
